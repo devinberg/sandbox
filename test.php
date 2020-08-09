f5f34d81462af5d2f587eb7616b43798f20f7b0b
Devin Berg <devinberg@gmail.com>
2020-08-08 21:31:44 -0500

    write a CSV file

--- test.php
+++ test.php
@@ -1,4 +1,9 @@
 <?php
 $val = getopt("p:");
 print(join(',', $val));
+$headers = array('GUID', 'Title', 'Abstract', 'status', 'file url', 'Download count', 'URL', 'Preprint DOI', 'Publisher DOI', 'Date created', 'Date modified');
+// Save the data to a CSV.
+$csv = fopen('engrxiv-papers.csv', 'a+');
+fputcsv($csv, $headers);
+fclose($csv);
 ?>
